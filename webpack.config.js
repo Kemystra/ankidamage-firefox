d8ded71dd138fde352b21762f6b5f75720bae716
k3mystra <programmer.coleslaw981@slmail.me>
2025-02-14 22:12:46 +0800

    Config webpack to just copy HTML and CSS files for now
    Processing them right now is quite overkill

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,18 @@
+const { default: FileManagerPlugin } = require('filemanager-webpack-plugin')
 const path = require('path')
+
+// Copy the file, without alteration
+// Planning to do so later, for now it's unnecessary complexity
+const FILE_MANAGER_OPTIONS = {
+    events: {
+        onEnd: {
+            copy: [
+                { source: "./src/popup/index.html", destination: "./built/popup/index.html" },
+                { source: "./src/popup/style.css", destination: "./built/popup/style.css" }
+            ]
+        }
+    }
+}
 
 module.exports = {
     // goddamn it why so complicated
@@ -6,7 +20,17 @@ module.exports = {
     entry: {
         "content_scripts/main.js": "./src/content_scripts/main.js",
         "background/card_maker.js": "./src/background/card_maker.js",
-        "popup/popup.html": "./src/popup/popup.html"
+        "popup/main.js": "./src/popup/main.js"
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+        ]
     },
 
     output: {
@@ -15,5 +39,7 @@ module.exports = {
 
         // Tell webpack to only show newly generated files
         clean: true
-    }
+    },
+
+    plugins: [new FileManagerPlugin(FILE_MANAGER_OPTIONS)]
 }
